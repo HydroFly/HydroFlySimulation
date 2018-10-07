6908019c0c550a135fcda4e1d7bf91dcd655ac3f
Russ Weas <russweas@gmail.com>
2018-10-07 02:12:51 -0500

    added git ignore

--- System.py
+++ System.py
@@ -1,8 +1,4 @@
 class System:
     ue = 0
-<<<<<<< HEAD
     def update_values(self):
-=======
-    def update_values():
->>>>>>> 942642e86c2ae08fe6473ccd3bdc19d30016b423
         self.ue = Calculator.exit_velocity(self.pressure, pipe_height)
--- simulationV2.py
+++ simulationV2.py
@@ -4,27 +4,28 @@
 
 from Grapher import Grapher
 from Calculator import Calculator
-from System import System
+# from System import System
 from PID_Controller import PIDController
 
 from numpy import *
 
 ############ Assumptions ###########
-mass_water = 18  # mass of water [kg]
-mass_dry = 0  # dry mass including stuctures and electronics... will update later
+mass_water = 20  # mass of water [kg]
+mass_dry = 0.2  # dry mass including stuctures and electronics... will update later
 mass_tot = mass_water + mass_dry
 mass_tot_new = mass_tot
 
 height = 0
 velocity = 0
 ALL_HELL_HAS_BROKE_LOSE = False
+ALL_HELL_2 = False
 
 gravity = -9.81
 rho_water = 997
 
 pressure = 500000  # Original pressure of pressurant [Pa] 0.5 MPa
-#volume_gas_orig = 3  # volume of pressurant [m^3]
-target_height = 2  # mission profile height [m]
+# volume_gas_orig = 3  # volume of pressurant [m^3]
+target_height = 2.1  # mission profile height [m]
 pipe_height = .5  # difference in height between nozzle and pressure tanks [m]
 
 nozzle_diam = 0.01  # [m]
@@ -33,12 +34,12 @@ nozzle_area = Calculator.nozzle_area(nozzle_diam)
 ###### Simulation Timing ######
 t_plus = 0
 dt_simulation = 0.01
-dt_physical = 0.01
-mission_end_time = 60
+dt_physical = 0.05
+mission_end_time = 120
 timermode2 = 0
 
 graph = Grapher()
-sys = System()
+# sys = System()
 PID = PIDController(1, 0, 0, dt_simulation)
 
 # Velocity from ascent PDR
@@ -49,82 +50,105 @@ PID = PIDController(1, 0, 0, dt_simulation)
 # hover mode
 
 mode = 1
+ue = sqrt(2 * (pressure / rho_water + gravity * pipe_height))
+m_dot_max = Calculator.m_dot(nozzle_area, ue)
 
-#potential_height = 0
+# potential_height = 0
 
 while t_plus <= mission_end_time:
     # Mode 1    = goto and maintain 2 meters    (target = 2m)
     # Mode 2  = start 10 second timer         (target = 2m)
     # Mode 3    = go back to ground             (target = 0m)
 
-    if height >=2:
+    if height >= target_height and mode == 1:
         mode = 2
-        #target_velocity = 0 #?
-        
+    # target_velocity = 0 #?
+
+    if mode == 1:
+        height_cv = PID.get_cv(target_height, height)  # target height - height
+
+        potential_height = (0.5 * ((velocity ** 2) / abs(gravity))) + height
+
+        if (potential_height <= target_height):
+            target_velocity = sqrt(9.81 * (target_height - height))
+        else:
+            target_velocity = 0
+
+        # delta-v = 2 delta-h / delta-t^2
+        # delta-h = height-cv - velocity * delta-t
+
+        tuning_time = dt_simulation*100
+
+        target_dv = 2 * (height_cv - velocity * tuning_time) / (tuning_time ** 2)
+        target_d_mass = mass_tot * exp((gravity * dt_simulation / ue) - (target_dv / ue))
+        m_dot_target = (mass_tot - target_d_mass) / dt_simulation
+
+        # PWM, PID
+        if Calculator.modulus(t_plus, dt_physical) == 0:
+            duty_cycle = (m_dot_target / m_dot_max)
+            if duty_cycle < 0:
+                duty_cycle = 0
+            if duty_cycle > 1:
+                duty_cycle = 1
+
+    # We hoverin
     if mode == 2:
-        timermode2 += dt_simulation
-        if timermode2 >= 10: #time to land
-            target_height = 0
-            mode = 3 #more of a debugger statement for now
-            
-            
-    height_cv = PID.get_cv(target_height, height) # target height - height
-    #height_cv = target_height - height
+        # if PID.times_cleaned == 0:
+        #     PID.clean()
+        target_height = 2
 
-    ue = sqrt(2 * (pressure / rho_water + gravity * pipe_height))
-    m_dot_max = Calculator.m_dot(nozzle_area, ue)
+        height_cv = PID.get_cv(target_height, height)  # target height - height
 
-    potential_height = (0.5*((velocity**2)/abs(gravity))) + height
-    print(height)
+        potential_height = (0.5 * ((velocity ** 2) / abs(gravity))) + height
 
-    if (potential_height <= target_height):
-        target_velocity = sqrt(9.81 * (target_height - height))
-    else:
-        target_velocity = 0
+        if (potential_height <= target_height):
+            target_velocity = sqrt(9.81 * (target_height - height))
+        else:
+            target_velocity = 0
 
-    # delta-v = 2 delta-h / delta-t^2
-    # delta-h = height-cv - velocity * delta-t
+        # delta-v = 2 delta-h / delta-t^2
+        # delta-h = height-cv - velocity * delta-t
 
-    tuning_time = 3
+        tuning_time = 3
 
-    target_dv = 2 * (height_cv - velocity * tuning_time) / (tuning_time ** 2)
-    target_d_mass = mass_tot * exp((gravity * dt_simulation / ue) - (target_dv / ue))
-    m_dot_target = (mass_tot - target_d_mass) / dt_simulation
-
-    # PWM, PID
-    if Calculator.modulus(t_plus, dt_physical) == 0:
-        duty_cycle = (m_dot_target / m_dot_max)
+        target_dv = 2 * (height_cv - velocity * tuning_time) / (tuning_time ** 2)
+        target_d_mass = mass_tot * exp((gravity * dt_simulation / ue) - (target_dv / ue))
+        m_dot_target = (mass_tot - target_d_mass) / dt_simulation
+        duty_cycle = m_dot_target / m_dot_max
+        if duty_cycle >1:
+            duty_cycle = 1
         if duty_cycle < 0:
             duty_cycle = 0
-        if duty_cycle > 1:
-            duty_cycle = 1
 
+
+    # MODE-INDEPENDENT CALC
     if mass_water <= 0:
         ALL_HELL_HAS_BROKE_LOSE = True
+        m_dot_max = 0
 
-        m_dot_max = 0  # oh shit, I'm about to fucking fall
-        mass_water = 0
     if ALL_HELL_HAS_BROKE_LOSE:
         m_dot_max = 0
+
+        if ALL_HELL_2 == False:
+            mission_end_time = t_plus + 10
+            ALL_HELL_2 = True
 
     m_dot = duty_cycle * m_dot_max
 
     mass_tot_new -= m_dot * dt_simulation
     mass_water -= m_dot * dt_simulation
 
-    dv = gravity * dt_simulation + ue * log(mass_tot / mass_tot_new)
-    mass_water -= m_dot * dt_simulation
-
+    dv = gravity * dt_simulation + (ue * log(mass_tot / mass_tot_new))  # if mass_tot = mass_tot_new, only gravity effects apply
+    asdf = mass_tot
     mass_tot = mass_tot_new
     velocity += dv
+    print("velocity", velocity)
     height += velocity * dt_simulation
 
     if height < 0:
         height = 0  # yes, the ground surprisingly exists and no it is not a sink hole
-        #height_flag = True
+        # height_flag = True
         velocity = 0
-
-    #potential_height = height + (velocity + dv)*dt_simulation
 
     t_plus += dt_simulation
     graph.record("height", height, t_plus, "Height", only_positive=True)
@@ -132,6 +156,6 @@ while t_plus <= mission_end_time:
     graph.record("duty_cycle", duty_cycle, t_plus, "Duty Cycle")
     graph.record("mass_water", mass_water, t_plus, "Mass of Water")
     graph.record("dv", dv, t_plus, "dv")
-    graph.record("potentail height", potential_height, t_plus, "potential height")
+    graph.record("potentail height", ue * log(asdf / mass_tot_new), t_plus, "asdfasdfasdf height")
 
 graph.show_plots()
